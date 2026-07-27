1d3bb038996943e8e70c986b72d81ef5b93eb857
Pratham <prathamgrover031@gmail.com>
2026-07-27 15:58:25 +0530

    Hide SECRET_KEY using python-dotenv

--- quizz/settings.py
+++ quizz/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(d$%@sw5y084+b_tki7coy8d33zn(-+1j+l!l-1@q%8i(-s)w'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q(d$%@sw5y084+b_tki7coy8d33zn(-+1j+l!l-1@q%8i(-s)w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
